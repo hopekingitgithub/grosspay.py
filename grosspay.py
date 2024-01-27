c3499d3746d315dc00d0dbe67111d409697b274c
ABEL WATOKA <144320969+hopekingitgithub@users.noreply.github.com>
2024-01-27 15:39:40 +0300

    Add files via upload

---
+++ grosspay.py
@@ -0,0 +1,11 @@
+# Gross Pay Calculator
+
+# Prompt the user for input
+hours_worked = float(input("Enter the number of hours worked: "))
+rate_per_hour = float(input("Enter the rate per hour: "))
+
+# Calculate the gross pay
+gross_pay = hours_worked * rate_per_hour
+
+# Display the result
+print(f"Gross Pay: Kshs. {gross_pay}")
---
+++ volume.py
@@ -0,0 +1,12 @@
+# Sphere Volume Calculator
+
+import math
+
+# Prompt the user for input
+radius = float(input("Enter the radius of the sphere: "))
+
+# Calculate the volume of the sphere
+volume = (4/3) * math.pi * radius**3
+
+# Display the result
+print(f"Volume of the sphere: {volume}")
